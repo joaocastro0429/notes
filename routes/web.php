fb9380b62d8e83f26e3d1d21d3d3d3304a76dc30
joaocastro <joaocastro0429@gmail.com>
2024-09-07 13:00:11 -0300

    criando a controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hello World";
 });
+
+Route::get('/about',function(){
+    return "About Page";
+
+});
+
+Route::get('/main',[MainController::class ,'index']);
